20ff17d500b1c0db1126212f3ed0c29a69fca9cc
Loisel Dimitry <dloisel002@iutbayonne.univ-pau.fr>
2022-03-07 11:29:05 +0100

    Update

---
+++ src/Entity/Cours.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursRepository::class)]
+class Cours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateHeureDebut;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateHeureFin;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(\DateTimeInterface $dateHeureDebut): self
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDateHeureFin(): ?\DateTimeInterface
+    {
+        return $this->dateHeureFin;
+    }
+
+    public function setDateHeureFin(\DateTimeInterface $dateHeureFin): self
+    {
+        $this->dateHeureFin = $dateHeureFin;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
